e4f84ec8c4b4e84bcb6feb47f89006fcac8c3e73
Anders Brakestad <35800041+Brakjen@users.noreply.github.com>
2025-09-26 11:05:10 +0000

    Initial commit of structure.

---
+++ .devcontainer/post_attach.sh
@@ -0,0 +1,10 @@
+source .devcontainer/bash_aliases.sh
+source .venv/bin/activate
+
+pre-commit install
+pre-commit run --all-files
+
+git config alias.s status
+git config alias.ss "status -s"
+
+pip install --upgrade pip
