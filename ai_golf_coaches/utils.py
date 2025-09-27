03d2a332c373f970b057c610ddf862f083ed17ae
Anders Brakestad <35800041+Brakjen@users.noreply.github.com>
2025-09-27 20:50:09 +0000

    doc utils.

--- ai_golf_coaches/utils.py
+++ ai_golf_coaches/utils.py
@@ -2,4 +2,10 @@ from pathlib import Path
 
 
 def get_git_root() -> Path:
+    """Get the root directory of the git repository.
+
+    Returns:
+        Path: The root directory of the git repository.
+
+    """
     return Path(__file__).parent.parent.resolve()
