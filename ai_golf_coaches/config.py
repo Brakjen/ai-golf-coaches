7b97a9918376babd2718990a64047bd3a36a22ef
Anders Brakestad <anders.brakestad@proton.me>
2025-09-30 13:22:44 +0200

    Use env file.

--- ai_golf_coaches/config.py
+++ ai_golf_coaches/config.py
@@ -152,6 +152,8 @@ class Settings(BaseSettings):
     )
 
     model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
         env_nested_delimiter="__",
         extra="ignore",
     )
